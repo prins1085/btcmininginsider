b1bf7ba597fba7f515cd9a22cac4f57a495d2f68
Prins Harkhani <109617216+prins1085@users.noreply.github.com>
2026-01-30 12:44:40 +0530

    add social bar ads

--- client/src/components/common/AdsterraNative.tsx
+++ client/src/components/common/AdsterraNative.tsx
@@ -1,15 +1,31 @@
+// "use client";
+// import { useEffect } from "react";
+
+// export default function AdsterraNative() {
+//   useEffect(() => {
+//     const script = document.createElement("script");
+//     script.src = "https://pl28605389.effectivegatecpm.com/43e07c1477fb7d10b52d14e79c61895f/invoke.js"; // your script URL
+//     script.async = true;
+//     script.setAttribute("data-cfasync", "false");
+
+//     document.body.appendChild(script);
+//   }, []);
+
+//   return <div id="container-43e07c1477fb7d10b52d14e79c61895f"></div>; // your container id
+// }
+
+
 "use client";
 import { useEffect } from "react";
 
 export default function AdsterraNative() {
   useEffect(() => {
     const script = document.createElement("script");
-    script.src = "https://pl28605389.effectivegatecpm.com/43e07c1477fb7d10b52d14e79c61895f/invoke.js"; // your script URL
+    script.src = "//plXXXX.highrevenuegate.com/XXXX/invoke.js";
     script.async = true;
     script.setAttribute("data-cfasync", "false");
-
     document.body.appendChild(script);
   }, []);
 
-  return <div id="container-43e07c1477fb7d10b52d14e79c61895f"></div>; // your container id
+  return <div id="container-43e07c1477fb7d10b52d14e79c61895f"></div>;
 }
